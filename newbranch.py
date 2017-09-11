792a06450ced8451183f4ac48e038193df39178f
Claire Veasey <cev4gx@virginia.edu>
2017-09-11 17:18:17 -0400

    Edit for Activity 4

--- newbranch.py
+++ newbranch.py
@@ -2,3 +2,4 @@ from helper.py import greeting
 if __name__ == "__main__":
     greeting("Hello")
     greeting("I am using the greeting function from helper.py")
+    greeting("I am adding a line so I can see the edit for Activity 4")
