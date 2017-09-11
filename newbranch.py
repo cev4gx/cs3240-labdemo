8a01e63920f7e848ba2d87dac166bf4f24caef0c
cev4gx <31742949+cev4gx@users.noreply.github.com>
2017-09-11 17:24:50 -0400

    Update newbranch.py

--- newbranch.py
+++ newbranch.py
@@ -3,3 +3,4 @@ if __name__ == "__main__":
     greeting("Hello")
     greeting("I am using the greeting function from helper.py")
     greeting("I am adding a line so I can see the edit for Activity 4")
+    greeting("Adding a second edit, also for Activity 4")
